35cb878899bf35915e1ec9c84f11257b73edaad9
JeffersonGrande <20220058@santacecilia.edu.sv>
2023-04-12 18:10:44 -0600

    quite todas las f

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="es">
 
 <head>
-  <title>Miravalle EcoSurfffff</title>
+  <title>Miravalle EcoSurf</title>
   <!--meta tags -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
