4ee27bd7e0a8422b808dfddce0a403fa8edec44e
Bondanza <20210024@santacecilia.edu.sv>
2023-04-29 19:18:39 -0600

    intento fallido de hacer el login registro

---
+++ log_Reg.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Sign up / Login Form</title>
+  <link rel="stylesheet" href="css/Log_Reg.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Slide Navbar</title>
+	<link rel="stylesheet" type="text/css" href="slide navbar style.css">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+</head>
+<body>
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
+
+			<div class="signup">
+				<form action="registrate.php" method="POST" >
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="usuario" placeholder="Nombre de Usuario">
+					<input type="email" name="correo" placeholder="Correo electronico">
+					<input type="password" name="password" placeholder="Contraseña">
+                    <input type="password" name="password2" placeholder="Repita su contraseña">
+					<input type="submit" placeholder="Sign up">
+
+                    <?php if(!empty($errores)): ?>
+				<div class="error">
+					<ul>
+						<?php echo $errores; ?>
+					</ul>
+				</div>
+			<?php endif; ?>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" required="">
+					<input type="password" name="pswd" placeholder="Password" required="">
+					<button>Login</button>
+				</form>
+			</div>
+	</div>
+</body>
+</html>
+<!-- partial -->
+  
+</body>
+</html>
